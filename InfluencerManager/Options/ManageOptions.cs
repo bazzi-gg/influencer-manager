0faee1b14a62c0b4ae494ef50aea661237f27da0
Plorence <ploseok@gmail.com>
2022-07-25 19:58:04 +0900

    create option class

---
+++ InfluencerManager/Options/ManageOptions.cs
@@ -0,0 +1,10 @@
+namespace InfluencerManager.Options;
+
+public class ManageOptions
+{
+    public const string ConfigurationKey = "Manage";
+    /// <summary>
+    /// ms
+    /// </summary>
+    public int LoopDelay { get; set; }
+}
